2f0fbf4294708edb655f8c26417c0cda3e6fe850
minthantko <minthantko101@gmail.com>
2025-09-24 20:01:29 +0700

    init folder

---
+++ src/main/java/se331/lab/AuctionItemBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionItemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionItemBackendApplication.class, args);
+    }
+
+}
