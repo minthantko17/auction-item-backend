1806b6600e27877ff82b70c5fe94fabbc296b95c
minthantko <minthantko101@gmail.com>
2025-09-25 19:08:19 +0700

    add corsConfig

--- src/main/java/se331/lab/AuctionItemBackendApplication.java
+++ src/main/java/se331/lab/AuctionItemBackendApplication.java
@@ -2,6 +2,9 @@ package se331.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AuctionItemBackendApplication {
@@ -10,4 +13,16 @@ public class AuctionItemBackendApplication {
         SpringApplication.run(AuctionItemBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .exposedHeaders("x-total-count");
+
+            }
+        };
+    }
 }
